a9bc0c6def80c573e36d9614343ca3a51a531dce
Soe Moe Stephen <soe@frontware.co.th>
2024-10-15 06:29:54 +0700

    fix: port changed

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 load_dotenv()
 
 class Settings(BaseSettings):
-    MONGODB_URI: str = os.getenv("MONGODB_URI")
+    MONGODB_URI: str = os.getenv("MONGODB_URI", "mongodb+srv://schnatr:vcS0AQDhqKWSi6Tc@clusterzero.6uzvv.mongodb.net/?retryWrites=true&w=majority&appName=ClusterZero")
     DB_NAME: str = os.getenv("DB_NAME", "USER")
     COLLECTION_NAME: str = os.getenv("COLLECTION_NAME", "USER")
     SECRET_KEY : str = os.getenv("SECRET_KEY", "testtest")
--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app.include_router(userController, prefix="/api/user")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8002, reload=True)
